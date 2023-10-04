9c2f29f1386cec4434a4cdee0a55f2e84c0eed21
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2023-10-05 00:39:27 +0530

    Update COVID-19 Data Exploration Queries.sql

--- COVID-19 Data Exploration Queries.sql
+++ COVID-19 Data Exploration Queries.sql
@@ -1,3 +1,11 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
+
 alter table ProjectPortfolio.coviddeaths modify column population bigint;
 
 delete from ProjectPortfolio.coviddeaths
